6bbf824246417fce93bb8f3424fc293cc7462a80
Brian Elting <belting01@gmail.com>
2017-07-12 17:42:26 -0700

    Updated smithy unit tests to use different deck counts

--- projects/eltingb/dominion/cardtest1.c
+++ projects/eltingb/dominion/cardtest1.c
@@ -11,11 +11,14 @@ int main() {
     int seed = 1000;
     int numPlayer = 2;
     int maxHandCount = 10;
+    int maxDeckCount = 10;
     int handCount;
+    int deckCount;
     struct gameState G;
     struct gameState testG;
     int k[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
     int testHand[10] = { smithy, silver, copper, adventurer, estate, duchy, village, smithy, copper, copper };
+    int testDeck[10] = { duchy, adventurer, estate, copper, minion, remodel, mine, copper, estate, estate };
     int expected;
     int isCurrentPlayer;
 
@@ -23,54 +26,72 @@ int main() {
 
     for (player = 0; player < numPlayer; player++) {
         for (handCount = 1; handCount < maxHandCount; handCount++) {
-            // Initialize game state
-            memset(&G, 23, sizeof(struct gameState));
-            initializeGame(numPlayer, k, seed, &G);
+            for (deckCount = 0; deckCount < maxDeckCount; deckCount++) {
+                // Initialize game state
+                memset(&G, 23, sizeof(struct gameState));
+                initializeGame(numPlayer, k, seed, &G);
 
-            // Initialize player hand
-            memcpy(&G.hand[player], &testHand, handCount * sizeof(int));
-            G.handCount[player] = handCount;
+                // Initialize cards
+                memcpy(&G.hand[player], &testHand, handCount * sizeof(int));
+                G.handCount[player] = handCount;
+                memcpy(&G.deck[player], &testDeck, deckCount * sizeof(int));
+                G.deckCount[player] = deckCount;
+                memcpy(&G.discard[player], &testDeck, (maxDeckCount - deckCount) * sizeof(int));
+                G.discardCount[player] = maxDeckCount - deckCount;
 
-            // Copy game state
-            memcpy(&testG, &G, sizeof(struct gameState));
+                // Copy game state
+                memcpy(&testG, &G, sizeof(struct gameState));
 
-            debug("Test player %d with %d hand cards, check smithy in hand before play: ", player, handCount);
-            assertEquals(G.hand[player][0], smithy);
+                debug("Test player %d with %d hand cards, %d deck cards, check smithy in hand before play: ", player, handCount, deckCount);
+                assertEquals(G.hand[player][0], smithy);
 
-            // Play Smithy card
-            playSmithy(&G, player, 0);
+                // Play Smithy card
+                playSmithy(&G, player, 0);
 
-            debug("Test player %d with %d hand cards, check smithy removed from hand after play: ", player, handCount);
-            assertNotEquals(G.hand[player][0], smithy);
+                debug("Test player %d with %d hand cards, %d deck cards, check smithy removed from hand after play: ", player, handCount, deckCount);
+                assertNotEquals(G.hand[player][0], smithy);
 
-            debug("Test player %d with %d hand cards, check last played card: ", player, handCount);
-            assertEquals(G.playedCards[G.playedCardCount - 1], smithy);
+                debug("Test player %d with %d hand cards, %d deck cards, check last played card: ", player, handCount, deckCount);
+                assertEquals(G.playedCards[G.playedCardCount - 1], smithy);
 
-            debug("Test player %d with %d hand cards, check played card count: ", player, handCount);
-            assertEquals(G.playedCardCount, testG.playedCardCount + 1);
+                debug("Test player %d with %d hand cards, %d deck cards, check played card count: ", player, handCount, deckCount);
+                assertEquals(G.playedCardCount, testG.playedCardCount + 1);
 
-            // Test hand and deck counts for all players
-            for (i = 0; i < numPlayer; i++) {
-                isCurrentPlayer = player == i;
+                // Test hand, deck and discard counts for all players
+                for (i = 0; i < numPlayer; i++) {
+                    isCurrentPlayer = player == i;
 
-                debug("Test player %d with %d hand cards, check player %d hand count: ", player, handCount, i);
+                    debug("Test player %d with %d hand cards, %d deck cards, check player %d hand count: ", player, handCount, deckCount, i);
 
-                expected = (isCurrentPlayer)
-                    ? testG.handCount[i] + 2
-                    : testG.handCount[i];
+                    expected = (isCurrentPlayer)
+                        ? testG.handCount[i] + 2
+                        : testG.handCount[i];
 
-                assertEquals(G.handCount[i], expected);
+                    assertEquals(G.handCount[i], expected);
 
-                debug("Test player %d with %d hand cards, check player %d deck count: ", player, handCount, i);
+                    debug("Test player %d with %d hand cards, %d deck cards, check player %d deck count: ", player, handCount, deckCount, i);
 
-                expected = (isCurrentPlayer)
-                    ? testG.deckCount[i] - 3
-                    : testG.deckCount[i];
+                    if (isCurrentPlayer) {
+                        expected = (deckCount < 3)
+                            ? maxDeckCount - 3
+                            : testG.deckCount[i] - 3;
+                    }
+                    else {
+                        expected = testG.deckCount[i];
+                    }
 
-                assertEquals(G.deckCount[i], expected);
+                    assertEquals(G.deckCount[i], expected);
+
+                    debug("Test player %d with %d hand cards, %d deck cards, check player %d discard count: ", player, handCount, deckCount, i);
+
+                    expected = (isCurrentPlayer && deckCount < 3)
+                        ? 0
+                        : testG.discardCount[i];
+
+                    assertEquals(G.discardCount[i], expected);
+                }
+
             }
-
-
         }
 
     }
